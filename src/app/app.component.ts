2ed293e34af93f1ce0b947c740fb2237a3df6315
Enas20Hussein <enashussein899@gmail.com>
2023-12-05 17:48:50 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef,Inject, Injectable, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ServiceService } from './shared/service.service';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { HttpClient } from '@angular/common/http';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'firstname',
+    'lastname',
+    'email',
+    'birthday',
+    'gender',
+    'education',
+    'company',
+    'experience',
+    'package',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   title = 'ReEmployee';
+  empform!: FormGroup<any>;
+  education: string[] = ['student', 'junior', 'senior', 'post graduate'];
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  constructor(private fb: FormBuilder,private service:ServiceService)
+  {
+      this.empform = this.fb.group({
+        firstname: '',
+        lastname: '',
+        email: '',
+        birthday: '',
+        gender: '',
+        education: '',
+        company: '',
+        experience: '',
+        package: '',
+      });
+    }
+    ngOnInit(): void {
+      this.getemployeelist();
+      
+  }
+ 
+  onformSubmit() {
+    if (this.empform.valid) {
+      this.service.addemployee(this.empform.value).subscribe({
+        next: (val: any) => {
+          alert('employee added successfully');
+          
+        },
+        error: (err: any) => {
+          alert(err);
+        },
+      });
+    }
+  }
+  getemployeelist() {
+    this.service.getallemployee().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => {
+        alert(err);
+      },
+    });
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  deleterowEmployee(id: number) {
+    this.service.deletemployee(id).subscribe({
+      next: (res) => {
+        alert('Employee is removed');
+        this.getemployeelist();
+      },
+      error: console.log,
+    });
+  }
+ 
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +31,26 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatProgressBarModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
